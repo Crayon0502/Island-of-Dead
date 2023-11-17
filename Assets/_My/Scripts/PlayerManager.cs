ef2115f15dc5f5bf922e0fbf61579965b19db358
Hyungmin Lee <149800543+Crayon0502@users.noreply.github.com>
2023-11-17 20:25:13 +0900

    2023/11/17(9)

---
+++ Assets/_My/Scripts/PlayerManager.cs
@@ -0,0 +1,33 @@
+using Cinemachine;
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    private StarterAssetsInputs input;
+
+    [Header("Aim")]
+    [SerializeField]
+    private CinemachineVirtualCamera aimCam;
+
+
+    void Start()
+    {
+        input = GetComponent<StarterAssetsInputs>();
+    }
+
+    void Update()
+    {
+        if(input.aim)
+        {
+            aimCam.gameObject.SetActive(true);
+        }
+        else
+        {
+            aimCam.gameObject.SetActive(false);
+        }
+    }
+}

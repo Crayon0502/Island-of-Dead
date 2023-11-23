0cf019bc2078bd8123467e71273d61300b6ae5c4
Hyungmin Lee <149800543+Crayon0502@users.noreply.github.com>
2023-11-23 21:35:46 +0900

    2023/11/23

--- Assets/_My/Scripts/PlayerManager.cs
+++ Assets/_My/Scripts/PlayerManager.cs
@@ -6,6 +6,7 @@ public class PlayerManager : MonoBehaviour
 {
     private StarterAssetsInputs input;
     private ThirdPersonController controller;
+    private Animator anim;
 
     [Header("Aim")]
     [SerializeField]
@@ -23,6 +24,7 @@ public class PlayerManager : MonoBehaviour
     {
         input = GetComponent<StarterAssetsInputs>();
         controller = GetComponent<ThirdPersonController>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -32,9 +34,28 @@ public class PlayerManager : MonoBehaviour
 
     private void AimCheck()
     {
+        if(input.reload)
+        {
+            input.reload = false;
+
+
+            if (controller.isReload)
+                return;
+
+            AimControll(false);
+            anim.SetLayerWeight(1, 1);
+            anim.SetTrigger("Reload");
+            controller.isReload = true;
+        }
+
+        if (controller.isReload)
+            return;
+
         if (input.aim)
         {
             AimControll(true);
+
+            anim.SetLayerWeight(1, 1);
 
             Vector3 targetPosition = Vector3.zero;
             Transform camTransform = Camera.main.transform;
@@ -55,10 +76,22 @@ public class PlayerManager : MonoBehaviour
             targetAim.y = transform.position.y;
             Vector3 aimDir = (targetAim - transform.position).normalized;
             transform.forward = Vector3.Lerp(transform.forward, new Vector3(aimDir.x, transform.forward.y, aimDir.z), Time.deltaTime * 180f);
+
+            if(input.shoot)
+            {
+                anim.SetBool("Shoot", true);
+            }
+            else
+            {
+                anim.SetBool("Shoot", false);
+            }
         }
         else
         {
             AimControll(false);
+
+            anim.SetLayerWeight(1, 0);
+            anim.SetBool("Shoot", false);
         }
     }
 
@@ -67,5 +100,12 @@ public class PlayerManager : MonoBehaviour
         aimCam.gameObject.SetActive(isCheck);
         aimImage.SetActive(isCheck);
         controller.isAimMove = isCheck;
+    }
+
+    public void Reload()
+    {
+        Debug.Log("dddd");
+        controller.isReload = false;
+        anim.SetLayerWeight(1, 0);
     }
 }

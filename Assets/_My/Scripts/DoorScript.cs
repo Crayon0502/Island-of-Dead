c7e0a519cd0ad83b4598de5ca3226d3231deebb0
Hyungmin Lee <149800543+Crayon0502@users.noreply.github.com>
2023-12-03 22:03:36 +0900

    2023/12/03

--- Assets/_My/Scripts/DoorScript.cs
+++ Assets/_My/Scripts/DoorScript.cs
@@ -6,20 +6,42 @@ public class DoorScript : MonoBehaviour
     public float openAngle = 90f;
     public float closeAngle = 0f;
     public float smooth = 2f;
+    public AudioClip openSound; 
+    public AudioClip closeSound; 
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
 
     public void OpenDoor()
     {
         isOpen = true;
+        PlayDoorSound(openSound);
     }
 
     public void CloseDoor()
     {
         isOpen = false;
+        PlayDoorSound(closeSound);
     }
 
     void Update()
     {
         Quaternion targetRotation = Quaternion.Euler(0, isOpen ? openAngle : closeAngle, 0);
         transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
+    }
+
+    void PlayDoorSound(AudioClip sound)
+    {
+        if (audioSource != null && sound != null)
+        {
+            audioSource.PlayOneShot(sound);
+        }
     }
 }
